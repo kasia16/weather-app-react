83c615c14137ac645ca01d561228b5746895d266
kasia16 <76659769+kasia16@users.noreply.github.com>
2021-04-25 19:19:07 +0100

    Updated App.js and index.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 
 export default function App() {
   return (
-  return (
     <div className="App">
       <Weather />
     </div>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './index.css';
 import App from './App';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
