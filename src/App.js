6fb45f9dde5f4d1a307ff73549a1686ee67a1f63
kasia16 <76659769+kasia16@users.noreply.github.com>
2021-04-25 22:27:47 +0100

    Deleted Bootstrap from index.html

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <Weather />
-    </div>
+    </div></div>
   );
 }
